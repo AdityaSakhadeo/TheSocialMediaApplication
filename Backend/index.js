69914f52489837055629d7127d14d89997aa7504
Samarth Pathak <pathaksamarth11@gmail.com>
2024-08-28 22:47:01 +0530

    Backend folder created

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 
-const PORT = process.env.PORT || 3000;s
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
     res.send('Backend Server running on :',PORT);
