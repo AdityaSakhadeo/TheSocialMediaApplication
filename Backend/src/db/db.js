49a7ea075122789e9131536af38e36d405ce1a6d
Samarth Pathak <pathaksamarth11@gmail.com>
2024-09-01 23:47:32 +0530

    database connected

---
+++ Backend/src/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+//Function to connect the database to the express APIs
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    console.log("\nDatabase connected successfully");
+    console.log(`\nDatabase host is:: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("Mongo DB connection error::: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
