0e1b8d08cf04288aba9ac58ad9e1813353907621
Aditya <aditya9850526874@gmail.com>
2024-09-02 21:49:52 +0530

    material UI added

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import App from './App.tsx';
+import './index.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', //We can adjust the theme as we want
+    },
+    secondary: {
+      main: '#dc004e', 
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
