5dbdbcf53b5905702615ec74658ed607ecd3fe48
Aditya <aditya9850526874@gmail.com>
2024-09-02 21:55:27 +0530

    final frontend commit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,13 +2,8 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from 'axios';
 function App() {
-  const [destinations, setDestinations] = useState<Destination[]>([]);
+  const [destinations, setDestinations] = useState([]);
 
-  interface Destination{
-    id:number,
-    name: string,
-    city:string;
-  }
   useEffect(() => {
     axios.get('/jokes')
       .then((response:any) => {
@@ -22,14 +17,8 @@ function App() {
 
   return (
     <>
+      <h1>Hello World</h1>
       <h1>There are {destinations.length} destinations available</h1>
-      {destinations.map((destination:Destination, index:number) => (
-          <div key={destination.id}>
-            <h2>{destination.name}</h2>
-            <p>City: {destination.city}</p>
-          </div>
-
-      ))}
     </>
   );
 }
