14b9dbd8bf9d04f39667e91cbeb41b9ee953bbfa
Aditya <aditya9850526874@gmail.com>
2024-09-30 17:20:39 +0530

    fixex the signup page

--- Frontend/src/screens/Signup.tsx
+++ Frontend/src/screens/Signup.tsx
@@ -13,21 +13,17 @@ import { Link as MuiLink,
 import { Google as GoogleIcon, Facebook as FacebookIcon, Twitter as TwitterIcon } from "@mui/icons-material";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import TravelGram from "../assets/TravelGram.jpg";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../redux/store/store";
-import { setLoading } from "../redux/slices/loaderSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../redux/store/store';
+import { setLoading } from '../redux/slices/loaderSlice';
+
+
 
 export default function Signup() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const isLoading = useSelector((state: RootState) => state.loader.isLoading);
-  const [credentials, setCredentials] = useState({
-    username: "",
-    mobile: "",
-    fullName: "",
-    email: "",
-    password: "",
-  });
+  const [credentials, setCredentials] = useState({ username: "", mobile: "", fullName: "", email: "", password: "" });
   const [mobileOrEmail, setMobileOrEmail] = useState(""); // Track the input separately
   const [errors, setErrors] = useState({username: "", password: ""});
   const theme = useTheme();
@@ -59,28 +55,29 @@ export default function Signup() {
   }, [isSmall]);
 
 
+  
+    
+    
+  
 
   const handleSignup = async (e: any) => {
     e.preventDefault();
     dispatch(setLoading(true));
 
-    const response = await fetch(
-      "http://localhost:4000/api/v1/users/register",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Allow-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify({
-          username: credentials.username,
-          email: credentials.email,
-          phoneNumber: credentials.mobile,
-          fullName: credentials.fullName,
-          password: credentials.password,
-        }),
-      }
-    );
+    const response = await fetch("http://localhost:4000/api/v1/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Allow-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        username: credentials.username,
+        email: credentials.email,
+        phoneNumber: credentials.mobile,
+        fullName: credentials.fullName,
+        password: credentials.password,
+      }),
+    });
 
     if (!response.ok) {
       const data = await response.json();
@@ -95,9 +92,9 @@ export default function Signup() {
       alert("User Created successfully, now you can log in---->")
       navigate("/");
     } else {
-      if (result.statusCode == 400) {
+      if (result.statusCode==400) {
         dispatch(setLoading(false));
-        alert(result.message[0]);
+        alert(result.message[0])
       }
       dispatch(setLoading(false));
     }
@@ -105,31 +102,31 @@ export default function Signup() {
 
   // Updated setSelectedFields to handle input properly
   const setSelectedFields = (input: string) => {
-    const mobile = /^[0-9]+$/;
+    const mobile = /^[0-9]+$/
     if (input.includes("@")) {
       setCredentials({ ...credentials, email: input, mobile: "" });
     } else if (mobile.test(input)) {
       setCredentials({ ...credentials, mobile: input, email: "" });
-    } else {
+    }
+    else{
       setCredentials({ ...credentials, email: input, mobile: "" });
     }
   };
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: any) => { 
     const { name, value } = e.target;
     setCredentials({ ...credentials, [name]: value });
-  };
+    };
 
   // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   //   setCredentials({ ...credentials, [e.target.name]: e.target.value });
   // };
 
-  const handleMobileOrEmailChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleMobileOrEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMobileOrEmail(e.target.value);
     setSelectedFields(e.target.value); // Pass the correct value here
   };
+
 
 return (
 
@@ -139,29 +136,43 @@ return (
       container direction="column"
       alignItems="center"
       justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      width="100vw"
-      bgcolor="#FFECEC"
-    >
-      <Grid2
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        border="1px solid grey"
-        boxShadow="0px 2px 4px rgba(0, 0, 0, 0.25)"
-        borderRadius="7px"
-        height="80vh"
-        bgcolor="#FFEDED"
+      border="1px solid grey"
+      boxShadow="0px 2px 4px rgba(0, 0, 0, 0.25)"
+      borderRadius="7px"
+      height="80vh"
+      bgcolor="#FFEDED"
       >
-        <Stack
-          width={"70%"}
-          height={"100vh"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          paddingInline={"50px"}
+        
+      
+
+
+
+      <Stack
+        width={"50%"}
+        height={"100vh"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        paddingInline={"60px"}
+        bgcolor="#FFEDED">
+
+        <img src={TravelGram} 
+          style={{
+            borderRadius: "40%",
+            boxShadow: "0 0 5px 0 #000000",
+            marginBottom: "20px",
+            width: "100px",
+            height: "100px"
+          }}
+          
+
+          alt="TravelGram Logo" />
+
+        <Typography
+          variant="h6"
+          fontSize={"20px"}
           bgcolor="#FFEDED"
+          color='orange'
+          justifyContent={"center"}
         >
           Welcome to TravelGram !!
         </Typography>
@@ -219,6 +230,7 @@ return (
             fullWidth
             sx={{backgroundColor:"#FFECEC"}}
           />
+        
         {/* Stack for the login button */}
       <Stack width={isSmall ? "80%" : "70%"} height={"auto"} marginBottom={"20px"}>
         <Button
@@ -243,35 +255,4 @@ return (
 );
 
 
-          {/* Signup Button */}
-          <Stack width={"100%"} height={"auto"}>
-            <Button
-              variant="contained"
-              onClick={handleSignup}
-              disableRipple
-              sx={{
-                backgroundColor: "#EBA51A",
-                color: "#FFFFFF", // Default text color
-                "&:hover": {
-                  backgroundColor: "#333333", // Background color on hover
-                },
-              }}
-              disabled={
-                !credentials.username ||
-                !credentials.password ||
-                credentials.password.length < 8 ||
-                isLoading
-              }
-            >
-              {isLoading ? (
-                <CircularProgress size={24} color="inherit" />
-              ) : (
-                "Login"
-              )}
-            </Button>
-          </Stack>
-        </Stack>
-      </Grid2>
-    </Box>
-  );
 }
